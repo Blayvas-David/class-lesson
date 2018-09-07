ef000b6e7382d20027b404b3db8e5041146b1ad7
MelonTart <43078613+MelonTart@users.noreply.github.com>
2018-09-07 14:20:46 -0700

    Add files via upload

---
+++ Marquard_Cole.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package henoi;
+
+/**
+ *
+ * @author cole.marquard
+ */
+public class Henoi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tower towers = new tower(64);
+        towers.solve();
+    }
+    
+}
+
+
+
