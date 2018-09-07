f56361d7e587c7e17f0b74adba34fec1ae4d244a
MorrisDominik <43078672+MorrisDominik@users.noreply.github.com>
2018-09-07 14:21:39 -0700

    Add files via upload

---
+++ Sources/Morris_Dominik.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sort.algorithms;
+import java.util.Random;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+/**
+ *
+ * @author dominik.morris
+ */
+public class timer {
+    public static Thread t;
+    public static int hcnt=0,mcnt=0,scnt=0,mscnt=0;
+    String str="",nstr="",mstr="",dstr="";
+    public static int cnt=0,cnt2=0;
+   public static int n = 6;
+    public static int timer()
+    {
+        
+      /* cnt++;
+              if(cnt==1)
+              {
+                  t.start();
+              }
+              else
+              {
+                  t.resume();
+              }
+*/
+ return 90;
+   
+    }
+}
